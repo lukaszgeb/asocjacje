aeb9a90f5cc4a5e1d9fc1b277e65128790808f1f
Lukasz Gebicki <geblukasz@gmail.com>
2020-02-02 17:18:32 +0100

    usuniete pytania

--- src/main/java/com/akademia/asocjacje/data/QuizData.java
+++ src/main/java/com/akademia/asocjacje/data/QuizData.java
@@ -19,33 +19,5 @@ public class QuizData implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-
-        Test test = new Test("About everything");
-        Question question = new Question("What is The Capital of Poland");
-        question.addAnswer(new Answer("Warsaw",true));
-        question.addAnswer(new Answer("Mumbai",false));
-        question.addAnswer(new Answer("Kolkatta",false));
-
-        Question question2 = new Question("Who is CEO of Tesla Motors?");
-        question2.addAnswer(new Answer("Bill Gates",false));
-        question2.addAnswer(new Answer("Steve Jobs",false));
-        question2.addAnswer(new Answer("Elon Musk",true));
-
-        Question question3 = new Question("Name World's Richest Man?");
-        question3.addAnswer(new Answer("Jeff Bazo",false));
-        question3.addAnswer(new Answer("Bill Gates",false));
-        question3.addAnswer(new Answer("Mark Zuckenberg",true));
-
-        Question question4 = new Question("Word's Longest River?");
-        question4.addAnswer(new Answer("River Nile",true));
-        question4.addAnswer(new Answer("River Amazon",false));
-        question4.addAnswer(new Answer("River Godavari",false));
-
-        test.addQuestion(question);
-        test.addQuestion(question2);
-        test.addQuestion(question3);
-        test.addQuestion(question4);
-
-        testRepository.save(test);
     }
 }
